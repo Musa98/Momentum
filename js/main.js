7a55fbd9d03ddb2d6c778d7a7a8a44189939e928
Musa98 <www.musik2014@mail.ru>
2020-10-24 10:47:45 +0300

    fix: Make 1 part of momentum.

--- js/main.js
+++ js/main.js
@@ -1,26 +1,32 @@
 //DOM ELEMENTS
+const monthDay = document.getElementById('month-day');
 const time = document.getElementById('time');
 const greeting = document.getElementById('greeting');
 const name = document.getElementById('name');
 const focus = document.getElementById('focus');
-const showAmPm = true;
+
+//const showAmPm = true;
 
 //VIEW TIME
 const showTime = () =>{
+    const week = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
+    const nameMonth = ['January','February','March','April','May','June','July','August','September','October','November','December'];
     let today = new Date();
+    let month = today.getMonth();
+    let day = today.getDate();
+    let dayWeek = today.getDay();
     let hour = today.getHours();
     let min = today.getMinutes();
     let sec = today.getSeconds();
 
     //Set AM or PM
-    const amPm = hour >= 12 ? 'PM' : 'AM';
+    // const amPm = hour >= 12 ? 'PM' : 'AM';
 
-    //12hr Format
-
-    hour = hour % 12 || 12;
 
     //Output Time
-    time.innerHTML = `${hour}<span>:</span>${addZero(min)}<span>:</span>${addZero(sec)} ${showAmPm ? amPm : ''}`
+    monthDay.innerHTML = `${week[dayWeek]}, ${day} ${nameMonth[month]}`;
+    time.innerHTML = `${addZero(hour)}<span>:</span>${addZero(min)}<span>:</span>${addZero(sec)}`
+    // ${showAmPm ? amPm : ''}
     setTimeout(showTime,1000);
 }
 
@@ -34,19 +40,20 @@ const addZero = (n) =>{
 const SetBgGreed = () =>{
     let today = new Date();
     let hour = today.getHours();
-    if(hour < 12){
+    if(hour > 6 && hour < 12){
         document.body.style.backgroundImage = "url('../Momentum/img/morning.jpg')";
-        greeting.textContent = 'Good Morning';
-    } else if(hour < 18) {
+        greeting.textContent = 'Good Morning,';
+        document.body.style.color = 'white';
+    } else if(hour > 12 && hour < 18) {
         document.body.style.backgroundImage = "url('../Momentum/img/day.jpg')";
-        greeting.textContent = 'Good Afternoon';
-    } else if(hour < 21){
+        greeting.textContent = 'Good Afternoon,';
+    } else if(hour > 18 && hour < 24){
         document.body.style.backgroundImage = "url('../Momentum/img/evening.jpg')";
-        greeting.textContent = 'Good Evening';
+        greeting.textContent = 'Good Evening,';
         document.body.style.color = 'white';
     } else{
         document.body.style.backgroundImage = "url('../Momentum/img/night.jpg')";
-        greeting.textContent = 'Good Night';
+        greeting.textContent = 'Good Night,';
         document.body.style.color = 'white';
     }
 }
@@ -57,7 +64,6 @@ const getName = () =>{
         name.textContent = '[Enter Name]';
     }else {
         name.textContent = localStorage.getItem('name');
-        console.log(localStorage);
     }
 }
 
@@ -70,9 +76,8 @@ const setName = (e) =>{
             name.blur();
         }
     } else{
-        localStorage.setItem('name', e.target.innerText);
+        name.textContent = localStorage.getItem('name');
     }
-    console.log(localStorage);
 }
 
 //GET FOCUS
@@ -93,12 +98,19 @@ const setFocus = (e) =>{
             focus.blur();
         }
     } else{
-        localStorage.setItem('focus', e.target.innerText);
+        focus.textContent = localStorage.getItem('focus');
     }
 }
 
+const refreshText = (e) =>{
+    e.target.textContent = ' ';
+    //e.target.focus();
+}
+//localStorage.setItem('focus', 'hello');
+name.addEventListener('click', refreshText);
 name.addEventListener('keypress', setName);
 name.addEventListener('blur', setName);
+focus.addEventListener('click', refreshText);
 focus.addEventListener('keypress', setFocus);
 focus.addEventListener('blur', setFocus);
 
